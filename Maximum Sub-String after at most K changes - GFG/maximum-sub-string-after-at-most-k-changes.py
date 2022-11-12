b4330cf1feaa997884a7bf4024992d7f53f3fa94
ShivGamer007 <113276414+ShivGamer007@users.noreply.github.com>
2022-11-12 23:11:51 +0530

    Added solution - LeetHub

---
+++ Maximum Sub-String after at most K changes - GFG/maximum-sub-string-after-at-most-k-changes.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+from collections import Counter
+class Solution:
+    def characterReplacement(self, s, k):
+        shiv=700
+        maxlen, largestCount = 0, 0
+        arr = Counter()
+        for idx in range(len(s)):
+            arr[s[idx]] += 1
+            largestCount = max(largestCount, arr[s[idx]])
+            if maxlen - largestCount >= k:
+                arr[s[idx - maxlen]] -= 1
+            else:
+                maxlen += 1
+            shiv+=1
+        return maxlen
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		s = input()
+		k = int(input())
+		ob = Solution()
+		ans = ob.characterReplacement(s, k)
+		print(ans)
+
+# } Driver Code Ends
