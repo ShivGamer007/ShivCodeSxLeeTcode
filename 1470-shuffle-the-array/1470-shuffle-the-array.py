0c21fd9664650c2ad417c7888fe007a06bba1257
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-02-06 19:10:05 +0530

    Time: 63 ms (70.68%), Space: 14.2 MB (33.52%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,15 +1,16 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        a=nums[:n]
-        b=nums[n:]
-        c=[]
-        i,j,k=0,0,0
-        while(i<2*n):
-            if i%2==0:
-                c.append(a[j])    
-                j+=1
-            else:
-                c.append(b[k])
-                k+=1
-            i+=1
-        return c
+        # a=nums[:n]
+        # b=nums[n:]
+        # c=[]
+        # i,j,k=0,0,0
+        # while(i<2*n):
+        #     if i%2==0:
+        #         c.append(a[j])    
+        #         j+=1
+        #     else:
+        #         c.append(b[k])
+        #         k+=1
+        #     i+=1
+        # return c
+        return list(chain(*zip(nums[:n],nums[n:])))
