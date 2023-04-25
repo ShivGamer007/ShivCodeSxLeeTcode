54db49f69cef6c32e860bf5f2b32a64486533502
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-04-26 00:26:38 +0530

    Time: 244 ms (24.72%), Space: 14.4 MB (88.19%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -0,0 +1,22 @@
+import heapq
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.pq=[]
+        for i in range(1,1001):
+            heapq.heappush(self.pq,i)
+
+    def popSmallest(self) -> int:
+        if self.pq:
+            return heapq.heappop(self.pq)
+        return -1
+
+    def addBack(self, num: int) -> None:
+        if num not in self.pq:
+            heapq.heappush(self.pq,num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
