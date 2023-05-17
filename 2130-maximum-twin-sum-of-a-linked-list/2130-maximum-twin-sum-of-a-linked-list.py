8f9f8a381a3c82a9e30db5e467da7be6a470c990
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-05-17 21:05:07 +0530

    Time: 837 ms (83.38%), Space: 47.5 MB (53.83%) - LeetHub

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+
+        def reverselinkedlist(head):
+            # temp=head
+            # if head==None or head.next==None: return 
+            # while(temp!= None and temp.next!=None and temp.next.next!=None):
+            #     temp.next.next=temp
+            #     temp=temp.next
+            # head.next=None
+            # head=temp
+            cur = head
+            prev = None
+            while(cur is not None):
+                next_node = cur.next
+                cur.next = prev
+                prev = cur
+                cur = next_node
+            return prev
+        def printll(head):
+            current = head
+            while current is not None:
+                print(current.val, end=" ")
+                current = current.next
+            
+        slow,fast=head,head
+        while(slow and fast and fast.next):
+            slow=slow.next
+            fast=fast.next.next
+        h2=slow
+        slow=None
+        head2 = reverselinkedlist(h2)
+        t1, t2 = head, head2
+        ans=0
+        # printll(head)
+        # print("ss")
+        # printll(head2)
+        while(t1 and t2):
+            sm = t1.val + t2.val
+            ans=max(ans,sm)
+            t1 = t1.next
+            t2 = t2.next
+        return ans
