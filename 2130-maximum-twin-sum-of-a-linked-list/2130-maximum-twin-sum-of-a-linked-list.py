33f273b57f903be5ba3eacd78cffc34b7c6941a3
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-05-17 21:11:04 +0530

    Time: 983 ms (27.64%), Space: 56.7 MB (20.82%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,35 +5,47 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-
-        def reverselinkedlist(head):
-            cur = head
-            prev = None
-            while(cur is not None):
-                next_node = cur.next
-                cur.next = prev
-                prev = cur
-                cur = next_node
-            return prev
-        def printll(head):
-            current = head
-            while current is not None:
-                print(current.val, end=" ")
-                current = current.next
-            
-        slow,fast=head,head
-        while(slow and fast and fast.next):
-            slow=slow.next
-            fast=fast.next.next
-        h2=slow
-        slow=None
-        head2 = reverselinkedlist(h2)
-        t1, t2 = head, head2
+        temp = head
+        arr=[]
+        while(temp):
+            arr.append(temp.val)
+            temp=temp.next
+        n=len(arr)
         ans=0
-        
-        while(t1 and t2):
-            sm = t1.val + t2.val
+        for i in range(n//2):
+            sm = arr[i] + arr[n-i-1]
             ans=max(ans,sm)
-            t1 = t1.next
-            t2 = t2.next
         return ans
+        
+        
+#         def reverselinkedlist(head):
+#             cur = head
+#             prev = None
+#             while(cur is not None):
+#                 next_node = cur.next
+#                 cur.next = prev
+#                 prev = cur
+#                 cur = next_node
+#             return prev
+#         def printll(head):
+#             current = head
+#             while current is not None:
+#                 print(current.val, end=" ")
+#                 current = current.next
+            
+#         slow,fast=head,head
+#         while(slow and fast and fast.next):
+#             slow=slow.next
+#             fast=fast.next.next
+#         h2=slow
+#         slow=None
+#         head2 = reverselinkedlist(h2)
+#         t1, t2 = head, head2
+#         ans=0
+        
+#         while(t1 and t2):
+#             sm = t1.val + t2.val
+#             ans=max(ans,sm)
+#             t1 = t1.next
+#             t2 = t2.next
+#         return ans
