3faab52277aeb9251073df1efab503e6272ae72f
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-07-15 23:57:08 +0530

    Time: 79 ms (32.92%), Space: 17.7 MB (46.21%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res = []
+        
+        def backtrack(sub="", i=0):
+            if len(sub) == len(s):
+                res.append(sub)
+            else:
+                if s[i].isalpha():
+                    backtrack(sub + s[i].swapcase(), i + 1)
+                backtrack(sub + s[i], i + 1)
+        
+        backtrack()
+        return res
