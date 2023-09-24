19ffa2138d87b7f806d41b50664837e5722437ca
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-09-24 23:47:56 +0530

    Added solution - LeetHub

---
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.py
@@ -0,0 +1,35 @@
+class Solution:
+    def duplicates(self, arr, n): 
+    	# code here
+    	
+    	ans = []
+    	
+    	for i in range(n):
+    	    idx = arr[i]%n
+    	    arr[idx] += n
+    	    
+    	for i in range(n):
+    	    if arr[i]//n>=2:
+    	        ans.append(i)
+    	
+    	if len(ans) == 0:
+    	    return [-1]
+    	return ans
+    	    
+
+
+#{ 
+ # Driver Code Starts
+if(__name__=='__main__'):
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        res = Solution().duplicates(arr, n)
+        for i in res:
+            print(i,end=" ")
+        print()
+
+
+
+# } Driver Code Ends
