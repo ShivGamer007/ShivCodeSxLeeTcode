6da5a3b28bd17559a9d259e1da96f9728960ca2c
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2023-09-25 16:49:14 +0530

    Time: 44 ms (41.02%), Space: 16.2 MB (76.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,21 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        cnt = {}
-        for i in s:
-            cnt[i] = cnt.get(i, 0)+1
-        for i in t:
-            cnt[i] = cnt.get(i, 0)-1
-        for i in cnt:
-            if cnt[i] == -1:
-                return i
-        return ""
+        c = 0
+        for i in range(len(s)):
+            c ^= ord(s[i])
+        for i in range(len(t)):
+            c ^= ord(t[i])
+            
+        return chr(c)
+        
+        
+        
+        # cnt = {}
+        # for i in s:
+        #     cnt[i] = cnt.get(i, 0)+1
+        # for i in t:
+        #     cnt[i] = cnt.get(i, 0)-1
+        # for i in cnt:
+        #     if cnt[i] == -1:
+        #         return i
+        # return ""
