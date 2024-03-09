334f6243070b09648f6a38c561c4e86d759ffca3
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-03-10 01:33:53 +0530

    Time: 376 ms (20.50%), Space: 50.6 MB (5.73%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,26 +1,25 @@
 class Solution:
     def getCommon(self, a: List[int], b: List[int]) -> int:
-        # mp = defaultdict(int)
-        # for n in a:
-        #     mp[n] += 1
-        # for n in b:
-        #     if mp[n] > 0:
-        #         return n
-        # return -1
+        mp = defaultdict(int)
+        for n in a:
+            mp[n] += 1
+        for n in b:
+            if mp[n] > 0:
+                return n
+        return -1
 
         
-        i, j = 0, 0
-        com = []
-        while i<len(a) and j<len(b):
-            if a[i] == b[j]:
-                com.append(a[i])
-                i += 1
-                j += 1
-            elif a[i]<b[j]:
-                i += 1
-            else:
-                j += 1
-        if len(com):
-            return min(com)
-        return -1
-        # return min(com) if min(com)!=None else -1
+#         i, j = 0, 0
+#         com = []
+#         while i<len(a) and j<len(b):
+#             if a[i] == b[j]:
+#                 com.append(a[i])
+#                 i += 1
+#                 j += 1
+#             elif a[i]<b[j]:
+#                 i += 1
+#             else:
+#                 j += 1
+#         if len(com):
+#             return min(com)
+#         return -1
