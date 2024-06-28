b33d5c03553b560850d70d8bbcb986da7469e5cc
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-06-28 23:56:27 +0530

    Time: 1210 ms (76.64%), Space: 42.1 MB (57.79%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.py
@@ -1,27 +1,13 @@
-from collections import defaultdict
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        mp = defaultdict(int)
-        for i in roads:
-            for j in i:
-                mp[j] += 1
-                
-        smp = dict(sorted(mp.items(), key = lambda x: x[1], reverse = True))
-        # print("smp = ", smp)
-        arr = list(smp.keys())
-        # print(arr)
-        
-        tmp = {}
-        x = n
-        for i in range(len(arr)):
-            tmp[arr[i]] = x
-            x -= 1
-        # print("tmp = ",tmp)
+        arr = [0] * (n)
         for road in roads:
-            road[0], road[1] = tmp[road[0]], tmp[road[1]]
-        
+            arr[road[0]] += 1
+            arr[road[1]] += 1
         ans = 0
-        # print(roads)
-        for i in roads:
-            ans += sum(i)
+        arr.sort(reverse = True)
+        # print(arr)
+        for i in range(len(arr)):
+            ans += arr[i] * n
+            n -= 1
         return ans
