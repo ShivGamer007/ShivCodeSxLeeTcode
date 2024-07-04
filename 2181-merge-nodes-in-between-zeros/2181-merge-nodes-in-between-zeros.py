36681e5771be849113fc3303e493a25d98004e6c
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-07-05 02:08:03 +0530

    Time: 782 ms (89.07%), Space: 56.3 MB (69.97%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -5,30 +5,53 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        x = []
-        tmp = head
-        p = []
+        tmp = head.next
+        prev = head
+        sm = 0
+        
+        
         while tmp != None:
             if tmp.val != 0:
-                p.append(tmp.val)
+                sm += tmp.val
             else:
-                x.append(sum(p))
-                p = []
+                tmp.val = sm
+                sm = 0
+                prev.next = tmp
+                prev = tmp
+                
             tmp = tmp.next
-        x = x[1:]
-        print(x)
-        if len(x) == 0:
-            return None
-        h1 = ListNode(x[0])
-        x.pop(0)
-        t1 = h1
-        while len(x):
-            t2 = ListNode(x[0])
-            x.pop(0)
-            t1.next = t2
-            t1 = t1.next
+                
+                
+        return head.next
         
         
-        return h1
+        
+        
+        
+#         x = []
+#         tmp = head
+#         p = []
+#         while tmp != None:
+#             if tmp.val != 0:
+#                 p.append(tmp.val)
+#             else:
+#                 x.append(sum(p))
+#                 p = []
+#             tmp = tmp.next
+#         x = x[1:]
+#         print(x)
+#         if len(x) == 0:
+#             return None
+#         h1 = ListNode(x[0])
+#         x.pop(0)
+#         t1 = h1
+#         while len(x):
+#             t2 = ListNode(x[0])
+#             x.pop(0)
+#             t1.next = t2
+#             t1 = t1.next
+        
+        
+#         return h1
             
                 
