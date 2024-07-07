320914fab0230eef2f70b55a0841de9c77786bb5
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-07-07 22:13:54 +0530

    Time: 41 ms (12.64%), Space: 16.6 MB (5.42%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.py
+++ 1518-water-bottles/1518-water-bottles.py
@@ -1,13 +1,17 @@
 class Solution:
     def numWaterBottles(self, bottle: int, ex: int) -> int:
-        if bottle < ex:
-            return bottle
-        ans = bottle
-        empty = bottle
         
-        while empty >= ex:
-            nw = empty//ex
-            ans += nw
-            empty = nw + (empty % ex)
+        extra = (bottle-1) // (ex-1) # cause you get one full bottle for every (ex-1) empty bottles
+        return bottle + extra
+    
+#         if bottle < ex:
+#             return bottle
+#         ans = bottle
+#         empty = bottle
         
-        return ans
+#         while empty >= ex:
+#             nw = empty//ex
+#             ans += nw
+#             empty = nw + (empty % ex)
+        
+#         return ans
