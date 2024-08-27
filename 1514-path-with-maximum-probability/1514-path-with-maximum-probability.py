0059a6a90b31391bfedbcd083723032287712cdb
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-08-27 20:58:01 +0530

    Time: 541 ms (71.87%), Space: 28.1 MB (88.74%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,27 +1,26 @@
 class Solution:
-    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        adj = [[] for _ in range(n)]
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        
+        adj = [[] for i in range(n)]
         for i in range(len(edges)):
             u, v = edges[i]
             p = succProb[i]
             adj[u].append((v, p))
             adj[v].append((u, p))
-
-        # Distances array
+            
         dist = [0.0] * n
-        dist[start] = 1.0
-
-        # Queue for BFS
-        queue = deque([start])
-
-        while queue:
-            curr = queue.popleft()
-
-            for node, prob in adj[curr]:
-                new_prob = dist[curr] * prob
-
+        dist[start_node] = 1
+        
+        q = deque([start_node])
+        while q:
+            cur = q.popleft()
+            for node, prob in adj[cur]:
+                new_prob = dist[cur] * prob
                 if new_prob > dist[node]:
                     dist[node] = new_prob
-                    queue.append(node)
-
-        return dist[end]
+                    q.append(node)
+        return dist[end_node]
+        
+        
+        
+        
