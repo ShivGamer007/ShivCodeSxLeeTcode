b24736136f6866dae57e7b5a783a0f25d4454eb1
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-09-09 21:52:15 +0530

    Time: 36 ms (49.84%), Space: 16.5 MB (78.48%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,36 +1,28 @@
 class Solution:
     def spiralOrder(self, mat: List[List[int]]) -> List[int]:
+        ans = []
+        m, n = len(mat), len(mat[0])
+        left, right, top, bottom = 0, n, 0, m
         
-        if not mat: return []
-        
-        res=[]
-        m=len(mat) #rows
-        n=len(mat[0]) #col
-        left,right,top,bottom=0,n-1,0,m-1
-
-        
-        while len(res) < (m*n):
+        while left<right and top<bottom:
             
-            #left to right in top row
-            for c in range(left,right+1):
-                res.append(mat[top][c])
-            top+=1
+            for i in range(left, right):
+                ans.append(mat[top][i])
+            top += 1
             
-            #top to bottom in right col
-            for r in range(top, bottom + 1):
-                res.append(mat[r][right])
-            right-=1
+            for i in range(top, bottom):
+                ans.append(mat[i][right-1])
+            right -= 1
             
-            #right to left in bottom row (reverse)
-            if top<=bottom:
-                for c in range(right, left-1,-1):
-                    res.append(mat[bottom][c])
-                bottom -= 1
+            if not(top<bottom and left<right):
+                break
             
-            #bottom to top in left col (reverse)
-            if left<=right:
-                for r in range(bottom, top-1, -1):
-                    res.append(mat[r][left])
-                left += 1
+            for i in range(right-1, left-1, -1):
+                ans.append(mat[bottom-1][i])
+            bottom -= 1
             
-        return res
+            for i in range(bottom-1, top-1, -1):
+                ans.append(mat[i][left])
+            left += 1
+        return ans
+            
