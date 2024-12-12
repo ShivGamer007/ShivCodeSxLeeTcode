e6c331298b0db9f73e31574b62c5b5ca95e35e18
Shivanshu Singh <113276414+ShivGamer007@users.noreply.github.com>
2024-12-12 22:06:03 +0530

    Time: 3 ms (87.72%), Space: 17.3 MB (20.60%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,7 +1,21 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        
-        for i in range(k):
-            mx = max(gifts)
-            gifts[gifts.index(mx)] = int((mx)**0.5)
-        return sum(gifts)
+    #using heap (using -ve for converting minheap operations into maxheap operations)
+    
+        gifts = [-i for i in gifts]
+        heapify(gifts)
+        x = 2**32
+        i = 0
+        while i < k and x > 1:
+            x = -heappop(gifts)
+            heappush(gifts, -int(x**0.5))
+            i += 1
+            
+        return -sum(gifts)
+    
+    
+    # Brute Force
+        # for i in range(k):
+        #     mx = max(gifts)
+        #     gifts[gifts.index(mx)] = int((mx)**0.5)
+        # return sum(gifts)
